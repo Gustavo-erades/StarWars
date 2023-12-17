778e4d7a44845266e0b1d2583bf3f4102cf6ff91
Gustavo-erades <eradesvilarinho@gmail.com>
2023-12-17 20:46:27 -0300

    começando página de créditos

---
+++ paginas/creditos.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css" media="all">
+    <link rel="stylesheet" href="../css/responsividade.css" media="all">
+    <link rel="stylesheet" href="../css/styleLoading.css">
+    <script src="https://kit.fontawesome.com/574d869779.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="../imagens/icone5.png" type="image/x-icon">
+    <title>Star Wars</title>
+  </head>
+  <body>
+   <div id="preload">
+      <!-- imagem parallax 1 -->
+      <div class="parallax img1" id="start">
+        <div class="conteudo-titulo">
+          <span class="titulo_logo text-warning display-1">Star Wars</span>
+        </div>
+        <br>
+        <!-- barra de navegação (menu hambúrger e dropdown) -->
+        <ul class="nav nav-pills justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link" href="../index.php">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle bg-transparent" data-toggle="dropdown" >Episódios</a>
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="../episodios/episodio1.php">Episódio I</a>
+              <a class="dropdown-item" href="../episodios/episodio2.php">Episódio II</a>
+              <a class="dropdown-item"  href="../episodios/episodio3.php">Episódio III</a>
+              <a href="./erro404.html" class="dropdown-item">Episódio IV</a>
+              <a href="./erro404.html" class="dropdown-item">Episódio V</a>
+              <a href="./erro404.html" class="dropdown-item">Episódio VI</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link active">Créditos</a>
+          </li>
+          <li class="nav-item">
+            <a href="../erro404.html" class="nav-link">Sobre</a>
+          </li>
+        </ul><!-- fim da barra de navegação -->
+      </div>
+      <!-- conteúdo card 1 -->
+      <div class="container conteudo">
+       
+      </div>
+      <!-- imagem parallax 2 -->
+      <div class="parallax img1">
+        <div class="conteudo-titulo">
+          <h1 class="titulo display-2">Divirta-se padawan!</h1>
+        </div>
+      </div>
+      <!-- conteúdo card 2-->
+      <div class="container conteudo">
+      </div>
+      <!-- imagem parallax 3 -->
+      <div class="parallax img1">
+        <div class="conteudo-titulo">
+          <h1 class="titulo display-2">Uma história Star Wars</h1>
+        </div>
+      </div>
+      <!-- conteúdo card 3 -->
+      <div class="container conteudo">
+      </div>
+      <!-- conteúdo parallax 4 -->
+      <div class="parallax img1">
+        <div class="conteudo-titulo">
+          <h1 class="titulo display-2">Trilogia Prequel</h1>
+        </div>
+      </div>
+      <!-- conteúdo card 4 -->
+      <div class="conteudo">
+       
+      </div>
+      <!-- conteúdo parallax 6 -->
+      <div class="parallax img1">
+        <div class="conteudo-titulo">
+          <h1 class="titulo display-2">Trilogia clássica</h1>
+        </div>
+      </div>
+      <!-- conteúdo card 6 -->
+      <div class="conteudo">
+        
+      </div>
+      <!-- conteúdo parallax 8 -->
+      <div class="parallax img1">
+        <div class="conteudo-titulo">
+          <h1 class="titulo display-2">A Força</h1>
+        </div>
+      </div>
+      <!-- conteúdo card 8 -->
+      <div class="container conteudo">
+        
+      </div>
+      <!-- conteúdo parallax 9 -->
+      <div class="parallax img1">
+        <div class="conteudo-titulo">
+          <h1 class="titulo display-2">Jedi x Sith</h1>
+        </div>
+      </div>
+      <!-- conteúdo card 9 -->
+      <div class="container conteudo">
+      
+       
+      </div>
+      <!-- conteúdo parallax 10 -->
+      <div class="parallax img1">
+        <div class="conteudo-titulo">
+          <h1 class="titulo display-2">Os sabres de luz</h1>
+        </div>
+      </div>
+      <!-- conteúdo card 10 -->
+      <div class="container conteudo">
+        
+      </div>
+      <!-- conteúdo parallax 11 -->
+      <div class="parallax img1">
+        <div class="conteudo-titulo">
+          <h1 class="titulo display-2">Que a Força esteja com você!</h1>
+        </div>
+      </div>
+      <div class="container">
+        <footer class="py-3 my-4">
+          <ul class="nav justify-content-center mb-3">
+            <li class="nav-item">
+              <a class="nav-link px-2 text-muted" href="https://github.com/Gustavo-erades" target="_blank"><i class="fa-brands fa-github fa-bounce" style="font-size:2.5em"></i></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link px-2 text-muted">&copy; 2023 por Gustavo Êrades </a>
+            </li>
+          </ul>
+          <div class="grid">
+            <div class="row">
+              <span id="span-footer-redes" class="col-xl-6">
+                fale comigo: 
+                <a href="https://www.linkedin.com/in/gustavo-%C3%AA-22a357231/" target="_blank" class="footer-redes"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="mailto:eradesvilarinho@gmail.com" target="_blank" class="footer-redes"><i class="fa-solid fa-envelope"></i></a>
+              </span>
+            </div>
+          </div>
+        </footer>
+      </div>
+      <a href="#start" id="de_volta_ao_topo">&#9650;</a>
+   </div>
+    <!-- loading -->
+    <div id="loading">
+        <img src="../imagens/icons8-r2d2-96.png" alt="Droide R2D2 passeando por aí">
+        <blockquote>
+            Carregando, por favor aguarde &#128512; 
+        </blockquote>
+    </div>
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/loading.js"></script>
+  </body>
+</html>
