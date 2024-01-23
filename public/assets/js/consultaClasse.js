bbe270f06331016b406087384b3c5e7532a73aa7
Gustavo-erades <eradesvilarinho@gmail.com>
2024-01-23 18:41:42 -0300

    alterando o javascript

--- public/assets/js/consultaClasse.js
+++ public/assets/js/consultaClasse.js
@@ -2,6 +2,8 @@ const url= new URLSearchParams(window.location.search);
 const pagina = urlParams.get("cod");
 const home= document.querySelector("#menuHome");
 const episodios= document.querySelector("#dropdown");
+console.log(url);
+console.log(pagina);
 switch (pagina){
     case '1':
         home.classList.toggle("active");
